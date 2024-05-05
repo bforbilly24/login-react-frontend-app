df2c93945da0b0e34d9fd8862e7a887e124544c7
bforbilly24 <halimputra0701@gmail.com>
2024-05-05 23:11:31 +0700

    feat(page): add /app/Login

---
+++ src/app/Login.jsx
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { images, users } from '../utils/constant';
+import CardBackgroundCover from '../components/card/card-background-cover';
+
+const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [emailError, setEmailError] = useState('');
+	const [submitError, setSubmitError] = useState('');
+	const [passwordError, setPasswordError] = useState(''); // add this line
+
+	const navigate = useNavigate();
+
+	const handleEmailChange = (e) => {
+		setEmail(e.target.value);
+		if (e.target.value && !e.target.value.includes('@')) {
+			setEmailError('Email must have @');
+		} else {
+			setEmailError('');
+		}
+	};
+
+	const handlePasswordChange = (e) => {
+		setPassword(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const user = users.find((user) => user.email === email);
+		if (!user) {
+			setSubmitError('Make sure your email is correct. 3 failed login attempts, your account will be disabled');
+		} else if (user.password !== password) {
+			setPasswordError('Password incorrect');
+		} else {
+			setEmailError('');
+			setPasswordError('');
+			setSubmitError('');
+			navigate('/success');
+		}
+	};
+
+	return (
+		<div className='grid h-screen w-full grid-cols-2'>
+			<CardBackgroundCover />
+			<div className='relative flex items-center justify-start bg-white'>
+				<div className='mx-[157.5px] flex w-full flex-col items-start justify-center gap-y-[25px]'>
+					<div className='flex w-full flex-col gap-y-[11px]'>
+						<h3 className='align-middle text-[26px] font-semibold leading-[26px] tracking-[-2%]'>Login</h3>
+						<p className='align-middle text-sm font-semibold leading-[14px] text-[#A1A5B7]'>Please log in to start using HRIS</p>
+					</div>
+
+					{/* Form Login */}
+					<form className='w-full space-y-[25px]' onSubmit={handleSubmit}>
+						{/* <label className='mb-2 block text-sm'>User name</label> */}
+						{/* <label className='mb-2 block text-sm'>Password</label> */}
+						<div className='flex flex-col gap-y-[10px]'>
+							<input name='username' type='email' required className={`w-full rounded-md border px-4 py-3 text-sm outline-1 ${emailError ? 'outline-[#D9214E]' : 'outline-[#00549B]'} `} placeholder='Email' value={email} onChange={handleEmailChange} />
+							{emailError && <p className='text-[13px] font-normal text-[#D9214E]'>{emailError}</p>}
+						</div>
+						{/* Password */}
+						<div className='flex flex-col gap-y-[10px]'>
+							<div className='relative flex items-center'>
+								<input name='password' type='password' required className={`w-full rounded-md border px-4 py-3 text-sm outline-1 ${passwordError ? 'outline-[#D9214E]' : 'outline-[#00549B]'}`} placeholder='Password' value={password} onChange={handlePasswordChange} />{' '}
+								{/* Hide */}
+                                <img src={images[2].path} alt={images[2].alt} srcSet={images[2].srcset} className='absolute right-4 h-6 w-6 cursor-pointer' />
+								{/* Show */}
+                                <img src={images[1].path} alt={images[1].alt} srcSet={images[1].srcset} className='absolute right-4 h-6 w-6 cursor-pointer' />
+							</div>
+							{passwordError && <p className='text-[13px] font-normal text-[#D9214E]'>{passwordError}</p>} {/* add this line */}
+						</div>
+
+						<div className='inline-flex items-center gap-x-[10px]'>
+							<label className='relative flex cursor-pointer items-center rounded-lg' htmlFor='customStyle'>
+								<input
+									type='checkbox'
+									className="before:content[''] peer relative h-8 w-8 cursor-pointer appearance-none rounded-lg border border-[#E1E3EA] bg-[#E1E3EA] transition-all before:absolute before:left-2/4 before:top-2/4 before:block before:h-12 before:w-12 before:-translate-x-2/4 before:-translate-y-2/4 before:rounded-lg before:bg-blue-500 before:opacity-0 before:transition-opacity checked:border-[#E1E3EA] checked:bg-[#0D5CC6] checked:before:bg-[#0D5CC6] hover:scale-105 hover:before:opacity-0"
+									id='customStyle'
+								/>
+								<span className='pointer-events-none absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100'>
+									<svg xmlns='http://www.w3.org/2000/svg' className='h-3.5 w-3.5' viewBox='0 0 20 20' fill='currentColor' stroke='currentColor'>
+										<path d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'></path>
+									</svg>
+								</span>
+							</label>
+							<p className='text-sm font-medium text-[#5E6278]'>Remember me</p>
+						</div>
+						{/* Button Login */}
+						<button type='submit' className='h-12 w-full rounded-lg bg-[#0D5CC6] text-sm font-medium leading-5 text-white  hover:bg-[#00549B] focus:outline-none'>
+							Login
+						</button>
+					</form>
+					{/* Error Handling for Wrong Email or Password */}
+					{submitError && <p className='w-[324px] text-[13px] font-normal text-[#D9214E]'>{submitError}</p>}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
