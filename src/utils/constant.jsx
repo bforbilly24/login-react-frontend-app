64b51a505646c2fcfb22bfc97254975d782ec7e0
bforbilly24 <halimputra0701@gmail.com>
2024-05-05 23:12:36 +0700

    feat(utils): add /utils/constant

---
+++ src/utils/constant.jsx
@@ -0,0 +1,35 @@
+const images = [
+	{
+		path: '/public/brand.svg',
+		alt: 'Brand Image',
+		srcset: '/public/brand.svg 1x, /public/brand@2x.svg 2x',
+	},
+    {
+		path: '/public/show.svg',
+		alt: 'Show',
+		srcset: '/public/show.svg 1x, /public/show@2x.svg 2x',
+	},
+    {
+		path: '/public/hide.svg',
+		alt: 'Hide',
+		srcset: '/public/hide.svg 1x, /public/hide@2x.svg 2x',
+	},
+];
+
+const users = [
+    {
+        email: 'belly@circleit.dev',
+        password: 'belly123',
+    },
+    {
+        email: 'supre@circleit.dev',
+        password: 'supre123',
+    },
+    {
+        email: 'bforbilly@circleit.dev',
+        password: 'bforbilly24',
+    },
+];
+
+
+export { images, users };
