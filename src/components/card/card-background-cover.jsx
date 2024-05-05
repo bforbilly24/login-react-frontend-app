1fb950d5919bba183254e552659d412a3469c6c2
bforbilly24 <halimputra0701@gmail.com>
2024-05-05 23:12:04 +0700

    feat(components): add /components/card/card-background-cover

---
+++ src/components/card/card-background-cover.jsx
@@ -0,0 +1,18 @@
+import { images } from '../../utils/constant';
+
+const CardBackgroundCover = () => {
+	return (
+		<div className='relative  flex items-center justify-start bg-[#00549B]'>
+			<div className='login-bg-cover absolute inset-0' />
+			<div className='mx-20 w-full gap-y-7 flex flex-col'>
+				<img src={images[0].path} alt={images[0].alt} srcSet={images[0].srcset} className='h-[134px] w-[113px]' />
+				<div>
+					<h4 className='text-white text-3xl line leading-login-125 font-bold'>Human Resource Integrated System</h4>
+					<p className='text-white text-xl leading-login-125 font-normal w-[430px] '>Empowering People, Streamlining Processes: Your HRIS Solution</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CardBackgroundCover;
