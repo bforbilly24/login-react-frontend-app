8879ab823bed8ccdd0cd42298a7cfc9e7b481991
bforbilly24 <halimputra0701@gmail.com>
2024-05-06 20:49:21 +0700

    style(core): update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,45 @@ export default {
 				sans: ['Inter', 'sans-serif'],
 			},
 			lineHeight: {
+				'login-14': '14px',
 				'login-125': '125%',
+			},
+			colors: {
+				'black-primary': '#181C32',
+				'blue-primary': '#00549B',
+				'blue-secondary': '#0D5CC6',
+				'red-primary': '#D9214E',
+				'grey-primary': '#A1A5B7',
+				'grey-secondary': '#5E6278',
+				'grey-teritary': '#E1E3EA',
+			},
+			gap: {
+				'login-10': '10px',
+				'login-11': '11px',
+				'login-25': '25px',
+			},
+			space: {
+				'login-25': '25px',
+			},
+			fontSize: {
+				'login-10': '10px',
+				'login-13': '13px',
+				'login-14': '14px',
+				'login-26': '26px',
+			},
+			height: {
+				'login-134': '134px',
+			},
+			width: {
+				'login-324': '324px',
+				'login-113': '113px',
+				'login-430': '430px',
+			},
+			letterSpacing: {
+				'login-2': '-2%',
+			},
+			margin: {
+				'login-157.5': '157.5px',
 			},
 		},
 	},
@@ -18,7 +56,7 @@ export default {
 		plugin(function ({ addUtilities }) {
 			addUtilities({
 				'.login-bg-cover': {
-					backgroundImage: "url('/public/background-cover.webp')",
+					backgroundImage: "url('/background-cover.webp')",
 					backgroundSize: 'cover',
 					opacity: '10%',
 				},
