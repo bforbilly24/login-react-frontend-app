f3f54bff78afcbbd7e3ba1c39f0dc5ca69ab022d
bforbilly24 <halimputra0701@gmail.com>
2024-05-06 20:50:39 +0700

    style(components): update custom utility classes

--- src/components/card/card-background-cover.jsx
+++ src/components/card/card-background-cover.jsx
@@ -2,13 +2,13 @@ import { images } from '@/utils/constant';
 
 const CardBackgroundCover = () => {
 	return (
-		<div className='relative  flex items-center justify-start bg-[#00549B]'>
-			<div className='login-bg-cover absolute inset-0' />
-			<div className='mx-20 w-full gap-y-7 flex flex-col'>
-				<img src={images[0].path} alt={images[0].alt} srcSet={images[0].srcset} className='h-[134px] w-[113px]' />
+		<div className='bg-blue-primary relative flex items-center justify-start'>
+			<div className='absolute inset-0 login-bg-cover' />
+			<div className='mx-20 flex w-full flex-col gap-y-7'>
+				<img src={images[0].path} alt={images[0].alt} srcSet={images[0].srcset} className='h-login-134 w-login-113' />
 				<div>
-					<h4 className='text-white text-3xl line leading-login-125 font-bold'>Human Resource Integrated System</h4>
-					<p className='text-white text-xl leading-login-125 font-normal w-[430px] '>Empowering People, Streamlining Processes: Your HRIS Solution</p>
+					<h4 className='line text-3xl font-bold leading-login-125 text-white'>Human Resource Integrated System</h4>
+					<p className='w-login-430 text-xl font-normal leading-login-125 text-white '>Empowering People, Streamlining Processes: Your HRIS Solution</p>
 				</div>
 			</div>
 		</div>
