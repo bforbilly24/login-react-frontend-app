9b02f844917943e38bbc73bf41abc61a0b9644ae
bforbilly24 <halimputra0701@gmail.com>
2024-05-07 18:11:50 +0700

    reafactor(page): update import as alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './app/Login';
-import Success from './app/Success';
+import Login from '@/app/Login';
+import Success from '@/app/Success';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/success" element={<Success />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Login />} />
+				<Route path='/success' element={<Success />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
